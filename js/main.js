ec1ea95c73d85a665c6698125143fa885c0e6400
zealchemy <zakatu2018@gmail.com>
2018-12-25 11:44:00 -0500

    create various class in main.js to add books to collection

--- js/main.js
+++ js/main.js
@@ -1,11 +1,75 @@
 // Book Class: Represents a Book
 
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
 // UI Class: For UI tasks
+
+class UI {
+    static displayBooks(){
+        const StoreBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '3434434'
+            },
+            {
+                title: 'Book Two',
+                author: 'Jane Doe',
+                isbn: '45545'
+            }
+        ];
+
+        const books = StoreBooks;
+
+        books.forEach(book => UI.addBookToList(book))
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete"><i class="fas fa-trash-alt"></i></i></a></td>
+        `;
+
+        list.appendChild(row);
+    }
+}
 
 //Store Class: For Storage
 
 // Event: Display Books
 
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
+
 // Event: Add a Book
+const submitBook = e => {
+    // prevent actual submit
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instantiate book
+    const book = new Book(title, author, isbn);
+
+    // Add book to UI
+    UI.addBookToList(book);
+
+    // Clear Fields
+    
+
+}
+document.querySelector('#book-form').addEventListener('submit', submitBook)
 
 // Event: Remove a Book
